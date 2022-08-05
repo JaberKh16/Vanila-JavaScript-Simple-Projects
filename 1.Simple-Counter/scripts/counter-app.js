05b45b8f6e406f4956ae1c4dd3160f5d14bfe096
Jaber Khan <md.jaberkhan66@gmail.com>
2022-08-05 22:37:31 +0600

    Counter-App

--- 1.Simple-Counter/scripts/counter-app.js
+++ 1.Simple-Counter/scripts/counter-app.js
@@ -3,3 +3,11 @@
 */
 
 'use strict';
+
+// getting input 
+let countInitial = document.getElementById('counter-input');
+let convertedToNum = parseInt(countInitial.value);
+
+// getting buttons
+const buttons = document.querySelectorAll('.btn') // returns a NodeList
+console.log(buttons);
