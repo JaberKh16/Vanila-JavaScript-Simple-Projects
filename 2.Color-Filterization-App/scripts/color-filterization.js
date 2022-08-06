fe3e482d166a6f2becd762bd6319b28f38819d91
Jaber Khan <md.jaberkhan66@gmail.com>
2022-08-06 16:20:53 +0600

    Color-Filterization-App

--- 2.Color-Filterization-App/scripts/color-filterization.js
+++ 2.Color-Filterization-App/scripts/color-filterization.js
@@ -9,7 +9,26 @@ const defaultColors = ['green', 'blue', 'red'];
 
 // getting the button
 const button = document.getElementById('color-button');
-// console.log(button);
+// console.log(button); // returns the button itself
 
 // getting the color value
-const colorOption = document.
+const colorOption = document.getElementsByClassName('color-value');
+// console.log(colorOption); // returns a HTMLCollection
+
+// including the event on clicking button
+button.addEventListener('click', function(event){
+    // generating random number from 0 to 2 because there are 3items in "defaultColor" array
+    const randomColor = getRandomNumber(); // calling the getDefaultColor() to get the colors
+    // document.body.style.backgroundColor = defaultColors[randomColor];
+    const backgroundWindow = document.getElementsByClassName('background-window');
+    console.log(backgroundWindow);
+    // colorOption.textContent = defaultColors[randomColor]
+;});
+
+// to generate value between 0 to 2
+function getRandomNumber(){
+    // defaultColors.length is multiplied with the Math.random()
+    // because Math.random() basically generate values between 0 to 1
+    // and multiplying gives values from 0 to default.length means 0 to 2 
+    return Math.floor(Math.random()* defaultColors.length);
+}
