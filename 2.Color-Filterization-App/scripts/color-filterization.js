2baaa4c7a6f33f3d9956094c6e5a6b07670a8613
Jaber Khan <md.jaberkhan66@gmail.com>
2022-08-06 14:17:30 +0600

    Color-Filterization-App

---
+++ 2.Color-Filterization-App/scripts/color-filterization.js
@@ -0,0 +1,15 @@
+/*
+    Color Filterization App
+*/
+
+'use strict';
+
+// setting the default RGB colors
+const defaultColors = ['green', 'blue', 'red'];
+
+// getting the button
+const button = document.getElementById('color-button');
+// console.log(button);
+
+// getting the color value
+const colorOption = document.
