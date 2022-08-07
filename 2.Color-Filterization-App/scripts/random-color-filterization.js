f270ac909bbbc1c3ebb862d209b4b0d94692b120
Jaber Khan <md.jaberkhan66@gmail.com>
2022-08-07 06:57:39 +0600

    Color-Filterization-App

--- 2.Color-Filterization-App/scripts/random-color-filterization.js
+++ 2.Color-Filterization-App/scripts/random-color-filterization.js
@@ -7,14 +7,14 @@
 
 
 // setting the all the avaible hex colors
-const colors = [0, 1, 2, 3, 4, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const colorsOption = [0, 1, 2, 3, 4, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
 // getting the button
 const button = document.getElementById('color-button');
 // console.log(button); // returns the button itself
 
 // getting the color value
-const colorOption = document.getElementsByClassName('color-value');
+const colorValue = document.getElementsByClassName('color-value');
 // console.log(colorOption); // returns a HTMLCollection
 
 // including the event on clicking button
@@ -24,22 +24,33 @@ button.addEventListener('click', function(event){
     // document.body.style.backgroundColor = defaultColors[randomColor];
     
     // setting the hex color symbol which has pound sign '#'
-    const hexSymbol = '#';
+    let hexSymbol = '#';
+    let hexColors = " " + hexSymbol; // to store the hex colors
+
+    // though hexcolor has 6 digits in it
+    for(let i = 0; i<6; i++){
+        hexColors += colorsOption[randomColor]
+    };
+    console.log(hexColors); // to see the generated hex colors
 
     // getting the backgroundWindow which returns a HTMLCollection
     const backgroundWindow = document.getElementsByClassName('background-window');
     Array.from(backgroundWindow).forEach(colorElement => {
         // changing the background color for each element of HTMLCollection
-        colorElement.style.backgroundColor = defaultColors[randomColor];
+        colorElement.style.backgroundColor = hexColors;
     });
     
-    // colorOption.textContent = defaultColors[randomColor]
+    // to get the color label in the background labeling
+    Array.from(colorValue).forEach(colorLabel=>{
+        colorLabel.innerText = hexColors;
+        console.log(colorLabel)
+    }); 
 ;});
 
-// to generate value between 0 to 2
+// to generate value between 0 to length of colorsOption array
 function getRandomNumber(){
     // defaultColors.length is multiplied with the Math.random()
     // because Math.random() basically generate values between 0 to 1
-    // and multiplying gives values from 0 to default.length means 0 to 2 
-    return Math.floor(Math.random()* defaultColors.length);
+    // and multiplying gives values from 0 to colorsOption.length   
+    return Math.floor(Math.random()* colorsOption.length);
 }
