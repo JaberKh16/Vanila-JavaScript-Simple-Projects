af2860a673bfebfbe22551e8d753814f77055a53
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-07 22:56:58 +0600

    Simple-Interest-Calculator-App

--- 3.Simple-Interest-Calculator/scirpt/simple-interest-calculator.js
+++ 3.Simple-Interest-Calculator/scirpt/simple-interest-calculator.js
@@ -5,8 +5,13 @@
 // defining the 'use strict' statement
 'use strict';
 
-// selecting 'principal-amount' input field
+// selecting 'principal-amount', 'no-of-years' and 'rate-of-interest' input fields
 const principalAmount = document.getElementById('principal-amount');
 const noOfYears = document.getElementById('no-of-years');
 const rateOfInterest = document.getElementById('rate-of-interest');
 
+// selecting the 'calculated-result' button element
+const buttonClicked = document.getElementById('calculated-result')
+buttonClicked.addEventListener('click', (principalAmount, noOfYears, rateOfInterest)=>{
+    
+}) 
