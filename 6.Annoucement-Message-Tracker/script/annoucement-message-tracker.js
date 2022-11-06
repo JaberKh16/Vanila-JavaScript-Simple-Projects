77fb95742cb373ce0abb1c3b73f2fd14fb4f06ec
Md. Jaber Khan <md.jaberkhan66@gmail.com>
2022-11-06 13:06:38 +0600

    Annoucement-Message-Tracker

---
+++ 6.Annoucement-Message-Tracker/script/annoucement-message-tracker.js
@@ -0,0 +1,19 @@
+function getTimerInfo(){
+    const timer = new Date();
+    document.getElementById("clock").innerHTML = timer.toLocalTimeString();
+}
+
+function getDaysInfo(){
+    const days = new Date();
+    const startDate = days.getDate();
+    const endDate = 7;
+    const remainingDays = endDate - startDate;
+    document.getElementById("remaining-days").innerHTML = remainingDays+ '' + 'days left';
+}
+
+// setting interval so that time changes in every 1sec
+setInterval(getTimerInfo, 1000);
+// setting timeout to avoid refreshing in every sec
+setTimeout(getTimerInfo, 0);
+// calling the getDaysInfo() to get the remaining days
+getDaysInfo();
